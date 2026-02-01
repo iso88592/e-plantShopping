8e6883548b78b20fdcd96bcce3ca5b3b887dbd60
Várkonyi Tibor Zoltán <varkonyi.tibor@njszg.hu>
2026-02-01 19:19:54 +0100

    Count quantities in cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -19,6 +19,14 @@ function ProductList({ onHomeClick }) {
         return cart.some(cartItem => cartItem.name === plant.name);
     }
 
+    const cartCount = () => {
+        let count = 0;
+        cart.forEach((item) => {
+            count += item.quantity;
+        });
+        return count;
+    }
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -269,7 +277,7 @@ function ProductList({ onHomeClick }) {
                             <circle cx="184" cy="216" r="12"></circle>
                             <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path>
                         </svg>
-                            <span className="cart_quantity_count">{cart.length < 1 ? "" : cart.length}</span>
+                            <span className="cart_quantity_count">{cartCount() < 1 ? "" : cartCount()}</span>
                     </a>
                 </div>
             </div>
